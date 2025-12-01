f562c40cbd52b6d3ac74bf0c475b68c83c77a1cf
Ssemenova2007 <sonechka05062007@gmail.com>
2025-12-02 00:00:45 +0700

    Delete Practicum5 directory

--- Practicum5/ex4.py
+++
@@ -1,9 +0,0 @@
-num=int(input())
-def parrots_number(num):
-    if num%10==1 and num//10!=1:
-        return "попугай"
-    elif num%10>1 and num%10<5 and num%10!=1:
-        return "попугая"
-    else:
-        return "попугаев"
-print(f"{num} {parrots_number(num)}")
--- Practicum5/ex6.py
+++
@@ -1,9 +0,0 @@
-first=int(input())
-second=int(input())
-third=int(input())
-if first==second==third:
-    print(3)
-elif (first==second and first!=second) or (first==third and first!=second) or (second==third and second!=first):
-    print(2)
-else:
-    print(0)
